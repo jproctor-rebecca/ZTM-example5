dd06e755cb6ce92c67716d89d06bc547f98e4595
Rebecca J Proctor <jproctor.m.ed.tn@gmail.com>
2020-12-23 21:35:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#developer fundamentals
+# don't read dictionary
+
+#operator precedence - order of operations
+print(20 - 3 * 4)
+#3*4 = 12; 20 - 12 = 8
+
+print((20 - 3) * 4)
+#20-3 = 17; 17*4=68
+
